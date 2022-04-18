4c13cebce14b8304578a8ce1b8a47e8245d5840e
Sanel Hadzini <31168388+theteladras@users.noreply.github.com>
2022-04-19 00:39:05 +0200

    chore: add requirement.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding="utf8") as f:
 
 MAJOR = 0
 MINOR = 12
-PATCH = 1
+PATCH = 2
 
 VERSION = '{}.{}.{}'.format(MAJOR, MINOR, PATCH)
 DESCRIPTION = 'String validation and sanitization'
