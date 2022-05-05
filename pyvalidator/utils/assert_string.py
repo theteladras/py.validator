f75f98dc4f3143b73c2fffd47176dc6537dfc065
Chad Roberts <jcbroberts@gmail.com>
2022-05-05 12:11:32 -0700

    should raise ValueError instead of a generic Exception

--- pyvalidator/utils/assert_string.py
+++ pyvalidator/utils/assert_string.py
@@ -6,6 +6,6 @@ def assert_string(input: str) -> String:
 
     if not is_string:
         invalid_type = type(input).__name__
-        raise Exception("Expected a string but received a {0}".format(invalid_type))
+        raise ValueError(f"Expected a string but received a {invalid_type}")
 
     return String(input)
