857cf1a98391d0df0f181b65981bf45fc88f2d52
Chad Roberts <jcbroberts@gmail.com>
2022-05-06 14:42:17 -0700

    Fixed a typo in a variable name

--- pyvalidator/is_credit_card.py
+++ pyvalidator/is_credit_card.py
@@ -7,9 +7,9 @@ def is_credit_card(input: str) -> bool:
     input = assert_string(input)
 
     credit_card = RegEx("^(?:4[0-9]{12}(?:[0-9]{3,6})?|5[1-5][0-9]{14}|(222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720)[0-9]{12}|6(?:011|5[0-9][0-9])[0-9]{12,15}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11}|6[27][0-9]{14}|^(81[0-9]{14,17}))$")
-    sanetization_pattern = RegEx("[- ]+", 'g')
+    sanitization_pattern = RegEx("[- ]+", 'g')
 
-    sanitized = input.sub(sanetization_pattern, '')
+    sanitized = input.sub(sanitization_pattern, '')
 
     if not credit_card.match(sanitized):
         return False
