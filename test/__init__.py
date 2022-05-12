67c8d7e5a18ba3d875e2a1323cfd7f6b30b571ee
Chad Roberts <jcbroberts@gmail.com>
2022-05-11 17:16:30 -0700

    improved logic to automatically separate any additional string with a space

--- test/__init__.py
+++ test/__init__.py
@@ -2,4 +2,7 @@ import inspect
 
 
 def print_test_ok(extra_string=None):
-    print(f"OK - {inspect.stack()[1][3]}{extra_string or ''}")
+    msg = f"OK - {inspect.stack()[1][3]}"
+    if extra_string:
+        msg += f" {extra_string}"
+    print(msg)
