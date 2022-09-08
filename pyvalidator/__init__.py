9d030f35f01a1da374b89ac59cacf79e60dd064a
Sanel Hadzini <31168388+theteladras@users.noreply.github.com>
2022-09-09 01:36:16 +0200

    feat: added is_license_plate validator

--- pyvalidator/__init__.py
+++ pyvalidator/__init__.py
@@ -61,3 +61,4 @@ from pyvalidator.is_uppercase import is_uppercase
 from pyvalidator.is_url import is_url
 from pyvalidator.is_uuid import is_uuid
 from pyvalidator.is_aws_arn import is_aws_arn
+from pyvalidator.is_license_plate import is_license_plate
