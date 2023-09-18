9ebd11cdf3ac6cfcb9f8505c0374538a58ead700
Andjela Dojcinovic <122420625+aangit@users.noreply.github.com>
2023-09-18 11:27:05 +0200

    feat: added is_mailto validator (#18)
    * added is_mailto validator and unittest
    
    * fix issues
    
    ---------
    
    Co-authored-by: Sanel Hadzini <31168388+theteladras@users.noreply.github.com>

---
+++ pyvalidator/is_mailto.py
@@ -0,0 +1,20 @@
+from .utils.assert_string import assert_string
+import re
+
+def is_mailto(input):
+    input = assert_string(input)
+    """
+    The function checks if a given string is a valid mailto link with one or more email addresses
+    separated by commas.
+
+    :param input: The input string that needs to be checked if it is a valid mailto link or not. A mailto
+    link is a hyperlink that opens the user's email client and pre-fills the "To" field with an email
+    address. The link starts with "mailto:" followed by one or more email addresses separated by commas.
+    :return: a boolean value (True or False) depending on whether the input string matches the
+    specified pattern and contains at least one comma-separated email address.
+    """
+    pattern = r'^mailto:([\w.-]+@[\w.-]+\.\w+)(?:,([\w.-]+@[\w.-]+\.\w+))*\??(subject=([\w%]+))?(?:(&)?cc=([\w.-]+@[\w.-]+\.\w+))?(?:(&)?bcc=([\w.-]+@[\w.-]+\.\w+))?(?:(&)?body=([\w,\.\-\_%]+))?$'
+    if re.match(pattern, input):
+        return True
+    return False
+
---
+++ test/test_is_mailto.py
@@ -0,0 +1,110 @@
+import unittest
+from pyvalidator.is_mailto import is_mailto
+from . import print_test_ok
+
+class TestIsValidMailtoLink(unittest.TestCase):
+    
+    def test_valid_mailto_link_with_single_email(self):
+        link = "mailto:john@example.com"
+        result = is_mailto(link)
+        self.assertTrue(result)
+    
+    def test_valid_mailto_link_with_multiple_emails(self):
+        link = "mailto:john@example.com,jane@example.com,jake@example.com"
+        result = is_mailto(link)
+        self.assertTrue(result)
+    
+    def test_invalid_mailto_link_missing_mailto_prefix(self):
+        link = "john@example.com"
+        result = is_mailto(link)
+        self.assertFalse(result)
+    
+    def test_invalid_mailto_link_missing_email(self):
+        link = "mailto:"
+        result = is_mailto(link)
+        self.assertFalse(result)
+    
+    def test_invalid_mailto_link_no_comma_separator(self):
+        link = "mailto:john@example.com jane@example.com"
+        result = is_mailto(link)
+        self.assertFalse(result)
+    
+    def test_invalid_mailto_link_invalid_email_format(self):
+        link = "mailto:john@example.com, jane@example,com"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_valid_mailto_link_for_multiple_emails(self):
+        link = "mailto:asd@asd.asd,asd@asd.de"
+        result = is_mailto(link)
+        self.assertTrue(result)
+
+    def test_invalid_mailto_link_for_multiple_emails_with_no_space_between(self):
+        link = "mailto:asd@asd.asd, asd@asd.de"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_contains_special_characters(self):
+        link = "mailto:alice@example.com?subject=Meeting^Agenda"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_contains_incomplete_subject(self):
+        link = "mailto:jane.smith@example.com?subject="
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_missing_monkey_char(self):
+        link = "mailto:john.doeexample.com"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_missing_mailto(self):
+        link = "john.doe@example.com"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_invalid_email(self):
+        link = "mailto:invalid-email"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_incorect_parameter_name(self):
+        link = "mailto:jane.smith@example.com?subj=Meeting%20Agenda"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_missing_email_address(self):
+        link = "mailto:?subject=Feedback"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_invalid_mailto_link_unencoded_chars(self):
+        link = "mailto:alice@example.com?subject=Meeting Agenda&body=Please%20bring*%20the%20documents"
+        result = is_mailto(link)
+        self.assertFalse(result)
+
+    def test_valid_mailto_link_for_multiple_emails_with_params(self):
+        link = "mailto:alice@example.com,bob@example.com,carol@example.com?subject=Project%20Team"
+        result = is_mailto(link)
+        self.assertTrue(result)
+
+    def test_valid_mailto_link_with_cc_and_bcc(self):
+        link = "mailto:support@example.com?cc=admin@example.com&bcc=manager@example.com"
+        result = is_mailto(link)
+        self.assertTrue(result)
+
+    def test_valid_mailto_link_with_cc(self):
+        link = "mailto:support@example.com?cc=admin@example.com"
+        result = is_mailto(link)
+        self.assertTrue(result)
+
+    def test_valid_mailto_link_with_bcc(self):
+        link = "mailto:support@example.com?bcc=admin@example.com"
+        result = is_mailto(link)
+        self.assertTrue(result)
+
+    def test_valid_mailto_link_with_bcc(self):
+        link = "mailto:feedback@example.com?body=Dear%20Support,%0AI%20have%20a%20question%20about%20your%20product."
+        result = is_mailto(link)
+        self.assertTrue(result)
